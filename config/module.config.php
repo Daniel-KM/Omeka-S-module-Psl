b309ba7173c059dd820016e8795970476c627cf1
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-04-20 00:00:00 +0200

    Added a form element to select media types.

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,11 @@
 namespace Psl;
 
 return [
+    'form_elements' => [
+        'factories' => [
+            Form\Element\MediaTypeSelect::class => Service\Form\Element\MediaTypeSelectFactory::class,
+        ],
+    ],
     'oaipmhrepository' => [
         'metadata_formats' => [
             'factories' => [
---
+++ src/Form/Element/MediaTypeSelect.php
@@ -0,0 +1,8 @@
+<?php
+namespace Psl\Form\Element;
+
+use Zend\Form\Element\Select;
+
+class MediaTypeSelect extends Select
+{
+}
